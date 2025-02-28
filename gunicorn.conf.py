e985172651652a0b92ce1fcb9fd1d11e83728a49
dmitray27 <dmitrayketz@gmail.com>
2025-02-28 12:48:27 +0300

    Create gunicorn.conf.py

---
+++ gunicorn.conf.py
@@ -0,0 +1,13 @@
+# gunicorn.conf.py
+
+# Основные параметры
+workers = 4
+worker_class = "gevent"
+bind = "0.0.0.0:10000"  # Порт Render по умолчанию
+timeout = 60  # Увеличиваем таймаут до 60 секунд
+
+# Логирование
+capture_output = True  # Перехват логов из stdout/stderr
+loglevel = "debug"     # Уровень детализации логов (debug, info, warning, error)
+accesslog = "-"        # Логировать запросы в консоль
+errorlog = "-"         # Логировать ошибки в консоль
