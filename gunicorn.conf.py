275933229a796302ea4a0cc743d15ddbef9d0ff7
dmitray27 <dmitrayketz@gmail.com>
2025-03-22 20:19:49 +0300

    Update gunicorn.conf.py

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,13 +1,13 @@
-# gunicorn.conf.py
-workers = 2
+workers = 1
 worker_class = "gevent"
 bind = "0.0.0.0:10000"
 timeout = 160
-keepalive = 75
-max_requests = 1000
-capture_output = True
-loglevel = "info"
-accesslog = "-"
-errorlog = "-"
-proxy_protocol = True
-forwarded_allow_ips = "*"
+preload = True
+# keepalive = 75
+# max_requests = 1000
+# capture_output = True
+# loglevel = "info"
+# accesslog = "-"
+# errorlog = "-"
+# proxy_protocol = True
+# forwarded_allow_ips = "*"
